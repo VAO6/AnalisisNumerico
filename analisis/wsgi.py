0b58b6b801b7d6ecf6a88596a98e9af434896f04
VAO6 <varisti6@gmail.com>
2020-11-25 00:23:47 -0500

    analisis

--- analisis/wsgi.py
+++ analisis/wsgi.py
@@ -1,7 +1,7 @@
 import os
 import sys
-sys.path.append('/opt/bitnami/apps/django/django_projects/AnalisisNumerico')
-os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/apps/django/django_projects/AnalisisNumerico/egg_cache")
+sys.path.append('/opt/bitnami/apps/django/django_projects/analisis')
+os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/apps/django/django_projects/analisis/egg_cache")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "analisis.settings")
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
