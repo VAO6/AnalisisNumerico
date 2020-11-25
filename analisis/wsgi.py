e4d110068b2959b997bdc41c886ddc5388de97e6
VAO6 <varisti6@gmail.com>
2020-11-24 23:01:56 -0500

    app

--- analisis/wsgi.py
+++ analisis/wsgi.py
@@ -1,16 +1,7 @@
-"""
-WSGI config for analisis project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
-"""
-
 import os
-
+import sys
+sys.path.append('/opt/bitnami/apps/django/django_projects/AnalisisNumerico')
+os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/apps/django/django_projects/AnalisisNumerico/egg_cache")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "analisis.settings")
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'analisis.settings')
-
 application = get_wsgi_application()
